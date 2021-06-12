dd6c98c2e4f9358a04b1208e2294508c64fb1b9b
Steven Oyer <contact.stevenoyer@gmail.com>
2021-06-12 12:34:08 +0200

    "Home" Ajout du mode dark

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,11 @@
 let search_bar = document.querySelector('#search-bar');
 let content_val = document.querySelector('#content-bar');
+let btn_dark = document.querySelector('.btn-dark-mode');
 
 search_bar.addEventListener('keyup', function() {
     content_val.innerHTML = search_bar.value;
+});
+
+btn_dark.addEventListener('click', function() {
+    document.documentElement.classList.toggle('dark-mode');
 });
